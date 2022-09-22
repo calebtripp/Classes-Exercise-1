11d8daf1c3b07f42defa2ba14fb81a102d911467
calebtripp <calebtripp@outlook.com>
2022-09-22 12:23:55 -0400

    done

---
+++ Classes Exercise 1/Classes Exercise 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Classes_Exercise_1;
+
+
+Car MGBGT = new Car();
+MGBGT.Make = "MG";
+MGBGT.Model = "BGT";
+MGBGT.Year = 1974;
+
+//Console.WriteLine(Car.MGBGT);
+
+Console.WriteLine(MGBGT.Make);
+Console.WriteLine(MGBGT.Model);
+Console.WriteLine(MGBGT.Year);  
+// Q is there a quicker way to print all, like cw car.mgbgt? 
+
+        
+        
+        
